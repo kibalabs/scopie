4a7fdf1550fbc2cb081f8eacca6a604630dbcd06
Krishan Patel <krishan@rockolabs.com>
2015-07-29 10:13:23 +0100

    Initial Commit

---
+++ MenuMirror/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  MenuMirror
+//
+//  Created by Krishan Patel on 29/07/2015.
+//  Copyright (c) 2015 Rocko Labs. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
